7020b0c1fc04b46796e9ebd950752f987324c24d
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-10 13:16:15 +0530

    updated: calling length function

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            Length.CalculateLength();
         }
     }
 }
